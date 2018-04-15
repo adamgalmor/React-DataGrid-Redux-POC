551d4a50b3a884b7007307c4a17e6ae67dfdbada
Adam Galmor <adamgalmor@users.noreply.github.com>
2018-04-15 11:24:00 +0300

    Added Rerdux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from "./components/App";
-import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from "./components/App";
+// import registerServiceWorker from './registerServiceWorker';
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    ), document.getElementById('root')
-);
-registerServiceWorker();
+// ReactDOM.render((
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>
+//     ), document.getElementById('root')
+// );
+// registerServiceWorker();
+
+////Redux Counter:
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './components/Counter'
+import counter from './reducers'
+
+const store = createStore(counter)
+const rootEl = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+    <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    rootEl
+)
+
+render()
+store.subscribe(render)
